a697d7e599cc642a0b6df550dfb29eb0ae62ea73
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-12-12 13:35:54 +0200

    Configure cluster to improve perfomance

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cluster from "cluster";
 import expressSession from "express-session";
+import os from "os";
 import routes from "../routes";
 import { monitorDevActions, handleErrors } from "../middlewares";
 
 dotenv.config();
+
+const numCPUs = os.cpus().length;
 
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
@@ -26,8 +30,24 @@ app.use(
   })
 );
 app.use(monitorDevActions);
-app.use("/", routes);
-app.use(handleErrors);
-app.listen(port, () => console.log(`listening on port ${port}`));
+if (cluster.isPrimary) {
+  console.log(`Number of CPUs is ${numCPUs}`);
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  app.use("/", routes);
+  app.use(handleErrors);
+  app.listen(port, () => console.log(`listening on port ${port}`));
+}
 
 export default app;
